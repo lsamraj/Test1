e486d9936eb5a739b5e64691140a6dba24611543
lsamraj <leosam.france@gmail.com>
2021-09-18 18:38:41 +0530

    commit1

---
+++ SeleniumExample1.java
@@ -0,0 +1,16 @@
+package com.pack1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumExample1 {
+
+	static WebDriver driver;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+driver = new ChromeDriver();
+driver.get("https://www.google.com/");
+
+	}
+
+}
